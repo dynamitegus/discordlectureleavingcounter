8decba00afd0090930bdb7422b579f88ca5fcb56
dynamitegus <dynamitegus@dynamitegus.org>
2025-04-16 14:28:58 +1000

    fix types and add imports

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+import fs from 'bun:fs';
+import path from 'bun:path';
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -8,7 +9,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient: { user: { tag: any; }; }) => {
+client.once(Events.ClientReady, (readyClient: Client<true>) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
