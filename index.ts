5231297fa3b7431b8d1cc29cbadfadb84291e849
dynamitegus <dynamitegus@dynamitegus.org>
2025-04-16 14:47:31 +1000

    Make the code work and i think its typesafe

--- index.ts
+++ index.ts
@@ -1,10 +1,30 @@
 import fs from 'bun:fs';
 import path from 'bun:path';
+import type { Client } from 'discord.js';
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+class ExtendedClient extends Client {
+
+    constructor() {
+        super({
+            intents: [
+                GatewayIntentBits.Guilds
+            ],
+            presence: {
+                activities: [{
+                    name: 'balls for balls'
+                }],
+                status: 'online'
+            }
+        })
+    }
+    
+}
+
+export const client: ExtendedClient = new ExtendedClient();
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
