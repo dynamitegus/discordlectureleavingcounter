1d5d5ceda5cf76816f13897963801805afdebe18
dynamitegus <dynamitegus@dynamitegus.org>
2025-04-16 15:06:52 +1000

    add command handler

--- index.ts
+++ index.ts
@@ -23,8 +23,29 @@ class ExtendedClient extends Client {
     }
     
 }
-
 export const client: ExtendedClient = new ExtendedClient();
+
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endswith('.ts'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+    }
+}
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
